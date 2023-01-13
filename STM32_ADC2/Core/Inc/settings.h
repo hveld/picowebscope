8d357d8683c07ee50404b580fb8430e7dab7ac91
JeroenSH <0982058@hr.nl>
2023-01-13 14:00:14 +0100

    added STM files

---
+++ STM32_ADC2/Core/Inc/settings.h
@@ -0,0 +1,15 @@
+#ifndef INC_SETTINGS_H_
+#define INC_SETTINGS_H_
+
+#include "processing.h"
+
+#define MAX_FILTER_TAPS 	64
+#define DMA_BUFFER_SIZE 	4000
+#define NO_TRIGGER_INDEX	DMA_BUFFER_SIZE + 1
+
+#define DEFUALT_TRIG_CH			1
+#define DEFAULT_TRIG_LVL		50
+#define DEFAULT_TRIG_EDGE		FALLING_EDGE
+#define DEFUALT_DECIMATION_I	4
+
+#endif /* INC_SETTINGS_H_ */
